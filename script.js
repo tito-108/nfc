0d87bf66d2e362019558bc8d30e1629250d94f10
tito-108 <itstarek8@gmail.com>
2024-10-10 02:45:48 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('introButton').addEventListener('click', function() {
+    const introText = document.getElementById('introText');
+    introText.classList.toggle('hidden');
+});
